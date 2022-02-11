53db2cbf8ed00b2df01988e9896c933550253f1c
cahuang10 <72476655+cahuang10@users.noreply.github.com>
2022-02-10 17:41:16 -0800

    sss

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,46 @@ import "../css/App.css";
 import data from "../sample_data.json";
 
 function App() {
-  return <div className="app">Trivia!</div>;
+  const [questionNum, setQuestionNum] = useState(0);
+
+  return (
+    <div className="app">
+      Trivia!
+      <Question
+        text={data[questionNum].question.text}
+        comp={<Answer answer={data[questionNum].question.choices} />}
+      />
+      <NextQuestion />
+      <Answer answer={data[questionNum].question.choices} />
+    </div>
+  );
+}
+function Question({ text }) {
+  return <div>{text}</div>;
+}
+function NextQuestion() {
+  return (
+    <div>
+      <button>next question</button>
+    </div>
+  );
+}
+function Answer({ answer }) {
+  return (
+    <div className="choices">
+      {answer.map((k) => (
+        <div>{k}</div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
+// Goal 1: Render the answer choices from sample_data.json on the screen.
+
+// - [ ] In App.jsx, create an Answer component.
+// - [ ] Render an instance of `<Answer />` inside of `<Question />`.
+// - [ ] Add props to `<Answer />` with the text "Answer goes here".
+//   - [ ] Pass props for the answer choices into `<Question />`.
+//   - [ ] Use those props to render the Answer components inside `<Question />` to display the answer choices.
+// - [ ] Refactor to use map to map over all answer choices.
